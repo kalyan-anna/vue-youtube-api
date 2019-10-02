86f1f6124896a7b9d800709b0146c873b1540ec3
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-02 18:40:22 +0930

    setup

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-center">
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput(event) {
+      this.$emit("termChange", event.target.value);
+    }
+  }
+};
+</script> 
+
+<style scoped>
+input {
+  width: 75%;
+}
+div {
+  margin: 20px;
+}
+</style>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <li class="list-group-item media" @click="onVideoSelect">
+    <img :src="thumnailUrl" />
+    <div class="media-body">
+      <span v-html="video.snippet.title"></span>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "VideoListItem",
+  props: {
+    video: Object
+  },
+  computed: {
+    thumnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  },
+  methods: {
+    onVideoSelect() {
+      this.$emit("videoSelect", this.video);
+    }
+  }
+};
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  cursor: pointer;
+}
+li:hover {
+  background-color: #eee;
+}
+</style>
